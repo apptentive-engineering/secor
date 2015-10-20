68ef5edaf96ee8d37a8b20960008606910053219
Stuart <stuart.owen@gmail.com>
2015-10-20 15:15:23 -0700

    Add tests for `OrgAppIdParser`

---
+++ src/test/java/com/pinterest/secor/parser/JsonOrgAppIdParserTest.java
@@ -0,0 +1,116 @@
+package com.pinterest.secor.parser;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.pinterest.secor.common.SecorConfig;
+import com.pinterest.secor.message.Message;
+
+/**
+ * Created by stuart on 10/20/15.
+ * Test the JSON App Id Parser
+ */
+
+@RunWith(PowerMockRunner.class)
+public class JsonOrgAppIdParserTest extends TestCase {
+
+    private SecorConfig mConfig;
+    private Message mCorrect1;
+    private Message mCorrect2;
+    private Message mMissingAppId;
+    private Message mMissingOrgId;
+    private Message mMissingDate;
+    private Message mInvalidDate;
+
+    @Override
+    public void setUp() throws Exception {
+        mConfig = Mockito.mock(SecorConfig.class);
+        Mockito.when(mConfig.getMessageTimestampName()).thenReturn("timestamp");
+
+        byte correct1[] = "{\"created_at\":1728000,\"conversation_id\":\"11543295\",\"label\":\"local#app#purchase\",\"app_id\":\"1234\",\"org_id\":\"abcd\"}"
+                .getBytes("UTF-8");
+        mCorrect1 = new Message("test", 0, 0, correct1);
+
+        byte correct2[] = "{\"created_at\":1727999,\"conversation_id\":\"11543295\",\"label\":\"local#app#purchase\",\"app_id\":\"1234\",\"org_id\":\"abcd\"}"
+                .getBytes("UTF-8");
+        mCorrect2 = new Message("test", 0, 0, correct2);
+
+        byte missingAppId[] = "{\"created_at\":1728000,\"conversation_id\":\"11543295\",\"label\":\"local#app#purchase\",\"org_id\":\"abcd\"}"
+                .getBytes("UTF-8");
+        mMissingAppId = new Message("test", 0, 0, missingAppId);
+
+        byte missingOrgId[] = "{\"created_at\":1728000,\"conversation_id\":\"11543295\",\"label\":\"local#app#purchase\",\"app_id\":\"1234\"}"
+                .getBytes("UTF-8");
+        mMissingOrgId = new Message("test", 0, 0, missingOrgId);
+
+        byte missingDate[] = "{\"conversation_id\":\"11543295\",\"label\":\"local#app#purchase\",\"app_id\":\"1234\",\"org_id\":\"abcd\"}"
+                .getBytes("UTF-8");
+        mMissingDate = new Message("test", 0, 0, missingDate);
+
+        byte invalidDate[] = "{\"created_at\":\"dog\",\"conversation_id\":\"11543295\",\"label\":\"local#app#purchase\",\"app_id\":\"1234\",\"org_id\":\"abcd\"}"
+                .getBytes("UTF-8");
+        mInvalidDate = new Message("test", 0, 0, invalidDate);
+    }
+
+    @Test
+    public void testExtractCorrect() throws Exception {
+        String[] path = new JsonOrgAppIdParser(mConfig).extractPartitions(mCorrect1);
+        assertEquals("org", path[0]);
+        assertEquals("abcd", path[1]);
+        assertEquals("app", path[2]);
+        assertEquals("1234", path[3]);
+        assertEquals("date", path[4]);
+        assertEquals("1970-01-21", path[5]);
+
+        path = new JsonOrgAppIdParser(mConfig).extractPartitions(mCorrect2);
+        assertEquals("org", path[0]);
+        assertEquals("abcd", path[1]);
+        assertEquals("app", path[2]);
+        assertEquals("1234", path[3]);
+        assertEquals("date", path[4]);
+        assertEquals("1970-01-20", path[5]);
+    }
+
+    @Test
+    public void testExtractMissingData() throws Exception {
+        String[] path = new JsonOrgAppIdParser(mConfig).extractPartitions(mMissingAppId);
+        assertEquals("org", path[0]);
+        assertEquals("abcd", path[1]);
+        assertEquals("app", path[2]);
+        assertEquals("None", path[3]);
+        assertEquals("date", path[4]);
+        assertEquals("1970-01-21", path[5]);
+
+        path = new JsonOrgAppIdParser(mConfig).extractPartitions(mMissingOrgId);
+        assertEquals("org", path[0]);
+        assertEquals("None", path[1]);
+        assertEquals("app", path[2]);
+        assertEquals("1234", path[3]);
+        assertEquals("date", path[4]);
+        assertEquals("1970-01-21", path[5]);
+
+        path = new JsonOrgAppIdParser(mConfig).extractPartitions(mMissingDate);
+        assertEquals("org", path[0]);
+        assertEquals("abcd", path[1]);
+        assertEquals("app", path[2]);
+        assertEquals("1234", path[3]);
+        assertEquals("date", path[4]);
+        assertEquals("0000-00-00", path[5]);
+
+    }
+
+    @Test
+    public void testExtractInvalidData() throws Exception {
+        String[] path = new JsonOrgAppIdParser(mConfig).extractPartitions(mInvalidDate);
+        assertEquals("org", path[0]);
+        assertEquals("abcd", path[1]);
+        assertEquals("app", path[2]);
+        assertEquals("1234", path[3]);
+        assertEquals("date", path[4]);
+        assertEquals("0000-00-00", path[5]);
+    }
+}
