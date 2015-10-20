1f452a5233037da42186dbae4aee57a4637437a0
Stuart <stuart.owen@gmail.com>
2015-10-20 15:15:42 -0700

    Add field for org

--- src/main/java/com/pinterest/secor/parser/OrgAppIdParser.java
+++ src/main/java/com/pinterest/secor/parser/OrgAppIdParser.java
@@ -7,28 +7,30 @@ import com.pinterest.secor.message.Message;
  * Created by stuart on 10/19/15.
  * Abstract App ID parser - extracts App ID and Date (YYYY-MM-DD) to use as partition names.
  */
-public abstract class AppIdParser extends MessageParser {
+public abstract class OrgAppIdParser extends MessageParser {
 
-    public AppIdParser(SecorConfig config) {
+    public OrgAppIdParser(SecorConfig config) {
         super(config);
     }
 
-    public class AppIdDate {
+    public class OrgAppIdDate {
+        private String orgId;
         private String appId;
         private String date;
 
-        public AppIdDate(String appId, String date) {
+        public OrgAppIdDate(String orgId, String appId, String date) {
+            this.orgId = orgId;
             this.appId = appId;
             this.date = date;
         }
 
         public String[] toArray() {
-            String[] array = {"app", appId, "date", date};
+            String[] array = {"org", orgId, "app", appId, "date", date};
             return array;
         }
     }
 
-    public abstract AppIdDate extractAppIdAndDate(final Message message);
+    public abstract OrgAppIdDate extractAppIdAndDate(final Message message);
 
     public String[] extractPartitions(final Message message) {
         return extractAppIdAndDate(message).toArray();
